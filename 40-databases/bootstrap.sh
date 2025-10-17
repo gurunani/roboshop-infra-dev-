9d7f3bfd7429fc0ba210cfe2d23e880bef64c316
Guru <nainiguruvinder@gmail.com>
2025-10-17 11:41:33 +0530

    Roboshop Infra

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -4,4 +4,4 @@
 # ansible-pull -U https://github.com/gurunani/ansible-roboshop-roles-.git -i localhost,  -e component=mongodb main.yaml
 component=$1
 dnf install ansible -y
-ansible-pull -U https://github.com/daws-84s/ansible-roboshop-roles-tf.git -e component=$1 -e env=$2 main.yaml
+ansible-pull -U https://github.com/gurunani/ansible-roboshop-roles-.git -e component=$1 -e env=$2 main.yaml
