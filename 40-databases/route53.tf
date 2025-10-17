ad3162ffd8e79e7572eb9b0a5a94ea21d6b2d191
Guru <nainiguruvinder@gmail.com>
2025-10-17 13:15:32 +0530

    Roboshop Infra

--- 40-databases/route53.tf
+++ 40-databases/route53.tf
@@ -1,41 +1,41 @@
-# Get the private hosted zone
-data "aws_route53_zone" "roboshop" {
-  name         = var.zone_name
-  private_zone = true
-}
+# # Get the private hosted zone
+# data "aws_route53_zone" "roboshop" {
+#   name         = var.zone_name
+#   private_zone = true
+# }
 
-# MongoDB Route53 Record
-resource "aws_route53_record" "mongodb" {
-  zone_id = data.aws_route53_zone.roboshop.zone_id
-  name    = "mongodb.${var.zone_name}"
-  type    = "A"
-  ttl     = 1
-  records = [aws_instance.mongodb.private_ip]
-}
+# # MongoDB Route53 Record
+# resource "aws_route53_record" "mongodb" {
+#   zone_id = data.aws_route53_zone.roboshop.zone_id
+#   name    = "mongodb.${var.zone_name}"
+#   type    = "A"
+#   ttl     = 1
+#   records = [aws_instance.mongodb.private_ip]
+# }
 
-# Redis Route53 Record
-resource "aws_route53_record" "redis" {
-  zone_id = data.aws_route53_zone.roboshop.zone_id
-  name    = "redis.${var.zone_name}"
-  type    = "A"
-  ttl     = 1
-  records = [aws_instance.redis.private_ip]
-}
+# # Redis Route53 Record
+# resource "aws_route53_record" "redis" {
+#   zone_id = data.aws_route53_zone.roboshop.zone_id
+#   name    = "redis.${var.zone_name}"
+#   type    = "A"
+#   ttl     = 1
+#   records = [aws_instance.redis.private_ip]
+# }
 
-# MySQL Route53 Record
-resource "aws_route53_record" "mysql" {
-  zone_id = data.aws_route53_zone.roboshop.zone_id
-  name    = "mysql.${var.zone_name}"
-  type    = "A"
-  ttl     = 1
-  records = [aws_instance.mysql.private_ip]
-}
+# # MySQL Route53 Record
+# resource "aws_route53_record" "mysql" {
+#   zone_id = data.aws_route53_zone.roboshop.zone_id
+#   name    = "mysql.${var.zone_name}"
+#   type    = "A"
+#   ttl     = 1
+#   records = [aws_instance.mysql.private_ip]
+# }
 
-# RabbitMQ Route53 Record
-resource "aws_route53_record" "rabbitmq" {
-  zone_id = data.aws_route53_zone.roboshop.zone_id
-  name    = "rabbitmq.${var.zone_name}"
-  type    = "A"
-  ttl     = 1
-  records = [aws_instance.rabbitmq.private_ip]
-}
+# # RabbitMQ Route53 Record
+# resource "aws_route53_record" "rabbitmq" {
+#   zone_id = data.aws_route53_zone.roboshop.zone_id
+#   name    = "rabbitmq.${var.zone_name}"
+#   type    = "A"
+#   ttl     = 1
+#   records = [aws_instance.rabbitmq.private_ip]
+# }
