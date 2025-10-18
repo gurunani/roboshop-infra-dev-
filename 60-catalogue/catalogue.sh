e855d4a106da108d00c4257f19061de800d8d804
Guru <nainiguruvinder@gmail.com>
2025-10-18 13:31:35 +0530

    R-I

--- 60-catalogue/catalogue.sh
+++ 60-catalogue/catalogue.sh
@@ -10,7 +10,7 @@ pip3 install --upgrade boto3 botocore
 export ANSIBLE_PYTHON_INTERPRETER=/usr/bin/python3
 
 # Explicitly set MONGODB_HOST
-ansible-pull -U https://github.com/gurunani/ansible-roboshop-roles-.git \
+ansible-pull -U https://github.com/gurunani/ansible-roboshop-roles-tf.git \
   -e component=$component \
   -e env=$env \
   -e MONGODB_HOST=mongodb-${env}.gurulabs.xyz \
