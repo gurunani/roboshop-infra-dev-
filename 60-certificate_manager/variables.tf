c78f1569225d5cd7fb037cc19b72c97312829af0
Guru <nainiguruvinder@gmail.com>
2025-10-18 16:29:30 +0530

    certificate-manager

---
+++ 60-certificate_manager/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "Roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z08007301TGLKBZ7OY820"
+}
+
+variable "zone_name" {
+    default = "gurulabs.xyz"
+}
