6ab5e8f406e46b18d41f314b02bfb60dcda04bb8
Guru <nainiguruvinder@gmail.com>
2025-10-19 14:06:38 +0530

    Roboshop-Infra-dev

--- 70-frontend-alb/data.tf
+++ 70-frontend-alb/data.tf
@@ -7,7 +7,7 @@ data "aws_ssm_parameter" "public_subnet_ids" {
 }
 
 data "aws_ssm_parameter" "frontend_alb_sg_id" {
-  name = "/${var.project}/${var.environment}/frontend_sg_id"
+  name = "/${var.project}/${var.environment}/frontend_alb_sg_id"
 }
 
 data "aws_ssm_parameter" "acm_certificate_arn" {
